bd003721f2b1a14e37362f57540c839b84f05dc8
George0717 <raden7george@gmail.com>
2022-10-22 09:26:57 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,33 @@
-const hello = document.getElementById('hello')
-hello.innerHTML = "Hello World"
+// Deklarasi
+let txtNpm = document.getElementById('npm')
+let txtNama = document.getElementById('name')
+let listMhs = document.getElementById('listMahasiswa')
+let data = [
+  
+]
+
+const buttonForm = document.getElementById('submit')
+buttonForm.addEventListener('click',function(){
+    console.log('Button Ditekan')
+    console.log(txtNama.value)
+    console.log(txtNpm.value)
+    data.push({'npm' : txtNpm.value,"nama": txtNama.value})
+    tampil()
+})
+
+function tampil(){
+    // clear elemen mhs
+    listMhs.innerHTML = ''
+    // gunakan foreach
+    data.forEach(listData)
+}
+function listData(item,index){
+    listMhs.innerHTML += item.npm + ' - ' + item.nama
+}
+
+
+
+
+
+
+
