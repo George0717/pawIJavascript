a265ff596d7918f32351bb13ede67e43112f5c35
George0717 <raden7george@gmail.com>
2022-10-27 09:30:38 +0700

    storage Done

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@
 let txtNpm = document.getElementById('npm')
 let txtNama = document.getElementById('name')
 let listMhs = document.getElementById('listMahasiswa')
+let tblMhs = document.getElementById('tblMahasiswa')
 let data = [] 
+tampil()
 
 
 const buttonForm = document.getElementById('submit')
@@ -10,10 +12,12 @@ buttonForm.addEventListener('click',function(){
     console.log('Button Ditekan')
     console.log(txtNama.value)
     console.log(txtNpm.value)
-    data.push({'npm' : txtNpm.value,"nama": txtNama.value})
+   
 
     // cek apakah ada data di dalam localStorage
     if(localStorage.getItem("listMahasiswa") === null){
+        // Simpant object ke arrayData
+        data.push({'npm' : txtNpm.value,"nama": txtNama.value})
         // Jika localStorage kosong/null
         // simpan localStorage dengan key listMahasiswa
         localStorage.setItem("listMahasiswa",JSON.stringify(data))
@@ -21,7 +25,12 @@ buttonForm.addEventListener('click',function(){
     }
     else{
         // Jika data sudah ada
+        // variabel dataLs dibuat agar data yang telah diisi tidak akan hilang
+        const dataLs = JSON.parse(localStorage.getItem("listMahasiswa"))
+        dataLs.push({"npm" : txtNpm.value,"nama": txtNama.value})
+        localStorage.setItem("listMahasiswa",JSON.stringify(dataLs))
         localStorage.getItem("listMahasiswa")
+       
     }
 
     tampil()
@@ -32,9 +41,14 @@ function tampil(){
     listMhs.innerHTML = ''
     // gunakan foreach
     data.forEach(listData)
+    // mengambil data localStorage dengan key listMahasiswa
+    const dataLs = JSON.parse(localStorage.getItem('listMahasiswa'))
+    dataLs.forEach(listData)
 }
 function listData(item,index){
     listMhs.innerHTML += '<li>' + item.npm + ' - ' + item.nama + '</li>'
+    
+    tblMhs.innerHTML += `<tr><td>${item.npm}</td> <td>${item.nama}</td> </tr>`  
 }
 
 
